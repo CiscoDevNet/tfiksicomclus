fa7ec725a0a2dee30a2db2bec045f27f8d0c16bc
prathjan <prathjan@cisco.com>
2022-04-12 20:14:45 -0700

    data inserted

--- main.tf
+++ main.tf
@@ -197,7 +197,7 @@ data "intersight_kubernetes_cluster_profile" "kubeprofaction" {
 
 #Wait for cluster to come up and then outpt the kubeconfig, if successful
 output "kube_config" {
-	value = intersight_kubernetes_cluster_profile.kubeprofaction.kube_config[0].kube_config
+	value = data.intersight_kubernetes_cluster_profile.kubeprofaction.kube_config[0].kube_config
 }
 
 
