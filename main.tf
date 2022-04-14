4442e12c0598d3a28dcbd474eb63a4b8e3989f93
prathjan <prathjan@cisco.com>
2022-04-14 09:43:25 -0700

    deploy cluster

--- main.tf
+++ main.tf
@@ -243,7 +243,7 @@ module "iks_cluster" {
   # Kubernetes Cluster Profile  Adjust the values as needed.
   cluster = {
     name                = local.clustername 
-    action              = "Unassign"
+    action              = "Deploy"
     wait_for_completion = true 
     worker_nodes        = 2
     load_balancers      = 5
